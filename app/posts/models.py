5c2dedfdcbf26de07ec70d1ff69db6cd53c065aa
Aditya Dangi <aditya.dangi@synechron.com>
2021-03-25 00:20:26 +0530

    initialized project and added posts app with post and vote model

---
+++ app/posts/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+class Post(models.Model):
+    """Model class for posts"""
+    title = models.CharField(max_length=255)
+    url = models.URLField()
+    poster = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+
+
+class Vote(models.Model):
+    """Model class for votes"""
+    voter = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
